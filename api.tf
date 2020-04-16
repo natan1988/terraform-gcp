2536527afbfd145a9016734e52479414ad8207b0
None <natan1988@bastion-fuchicorp.us-central1-a.c.sunlit-monolith-271503.internal>
2020-04-16 03:16:00 +0000

    added var

--- api.tf
+++
@@ -1,6 +0,0 @@
-resource "google_project_service" "project" {
-  project = "arctic-surf-269300"
-  service = "iam.googleapis.com"
-
-  disable_dependent_services = true
-}
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("var.service_account_file")}"
+  credentials = "${file("${var.service_account_file}")}"
   project     = "${var.google_project_id}"
   region      = "us-central1"
   version     = "2.12.0"
